9ef772b6ed30687adf9b5976847d8bdd4613bc19
heycker lima <heycker.paula@edu.senai.br>
2026-05-21 01:14:07 +0000

    Atividade 3: Tarefa 5:Faz a requisição HTTP e processa a resposta:get Agora, a função de nível superior para package:http fazer uma GETrequisição HTTP para a URL que acabamos  de construir. A awaitpalavra-chave  pausa a execução de  getWikipediaArticle até que a getchamada seja concluída e retorne um http.Response objeto.

--- cli.dart
+++ cli.dart
@@ -154,7 +154,7 @@ Codigo:
 
 import 'dart:io';
 import 'package:http/http.dart' as http; // Add this line
-const version = '0.0.12';
+const version = '0.0.3';
 
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.first == 'help') {
@@ -208,7 +208,7 @@ Comando: dart bin/cli.dart help
 Voce deve ver: The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'
 
 Comando: dart bin/cli.dart version
-Voce deve ver: Dartpedia CLI version 0.0.12
+Voce deve ver: Dartpedia CLI version 0.0.3
 
 Comando: dart bin/cli.dart search Dart Programming
 Voce deve ver: Looking up articles about "Dart Programming". Please wait.
@@ -218,3 +218,85 @@ Here ya go!
 Comando: dart bin/cli.dart search
 Voce deve ver: Please provide an article title.
 Flutter Framework
+-------------------------------------------------------------------------------
+
+Versao 0.0.4
+
+Codigo:
+
+ 
+
+import 'dart:io';
+import 'package:http/http.dart' as http; // Add this line
+const version = '0.0.4';
+
+void main(List<String> arguments) {
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    // Add this new block:
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
+    printUsage();
+  }
+} 
+
+void printUsage() {
+  print("The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'");
+}
+
+   Future<String> getWikipediaArticle(String articleTitle) async {
+   final url = Uri.https(
+    'en.wikipedia.org', 
+    '/api/rest_v1/page/summary/$articleTitle',
+  );
+   final response = await http.get(url); // Make the HTTP request
+
+  if (response.statusCode == 200) {
+    return response.body; // Return the response body if successful
+  }
+
+  // Return an error message if the request failed
+  return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
+}
+
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    // Await input and provide a default empty string if the input is null.
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    // Otherwise, join the arguments into the CLI into a single string
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle")');
+}
+
+Saida padrao ao executar o codigo:
+
+Comando: dart bin/cli.dart
+Voce deve ver: The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'
+
+Comando: dart bin/cli.dart help
+Voce deve ver: The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'
+
+Comando: dart bin/cli.dart version
+Voce deve ver: Dartpedia CLI version 0.0.4
+
+Comando: dart bin/cli.dart search Dart Programming
+Voce deve ver: Looking up articles about "Dart Programming". Please wait.
+Here ya go!
+(Pretend this is an article about "Dart Programming")
+
+Comando: dart bin/cli.dart search
+Voce deve ver: Please provide an article title.
+Flutter Framework
+
